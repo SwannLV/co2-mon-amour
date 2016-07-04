a3bd6374a5ec4fd19c411e113f2941ea57094547
swannlv <le.vigouroux.swan@gmail.com>
2016-07-05 00:16:56 +0200

    count on icon

--- src/bg/background.js
+++ src/bg/background.js
@@ -11,7 +11,7 @@
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });*/
-
+/*
 var img = new Image();
 img.onload = function () {
   LoadIcon();
@@ -20,15 +20,28 @@ img.src = "../../icons/icon19.png";
 
 function LoadIcon(){
   var canvas = document.createElement('canvas');
-  canvas.id     = "CursorLayer";
   canvas.width  = 19;
   canvas.height = 19;
   canvas.style.zIndex   = 8;
   canvas.style.position = "absolute";
   var context = canvas.getContext('2d');
+  //draw
   context.drawImage(img, 0, 0);
+  context.font = "10px Comic Sans MS";
+  //context.fillText("5",0,0,19);
+  context.fillStyle = "white";
+  context.textAlign = "left";
+  context.fillText("10", 1, 18);
   var imageData = context.getImageData(0, 0, 19, 19);
   chrome.browserAction.setIcon({
     imageData: imageData
   });
-}
+}*/
+
+chrome.browserAction.setBadgeText({
+  text: "10+",/*,
+  color: [0,255,0,255]*/
+});
+chrome.browserAction.setBadgeBackgroundColor({
+  color: [0,255,0,255]
+});
