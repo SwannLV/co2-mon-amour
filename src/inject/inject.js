909a711afd7a9e7435bff1dbd5fd23e84ec2ad36
swannlv <le.vigouroux.swan@gmail.com>
2016-07-09 23:17:04 +0200

    123

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -12,20 +12,30 @@ chrome.extension.sendMessage({}, function(response) {
 	}, 10);
 });
 
+var c_localStorageKey_co2 = "co2";
 
+function AddCo2(co2ToAdd)
+{
+	localStorage[c_localStorageKey_co2] = parseInt(localStorage[c_localStorageKey_co2]) + co2ToAdd;
+}
 
 $(function(){
-
+	
 	//page loaded
+	AddCo2(1);
 	console.log("CO2 : PAGE LOAD DETECTED : " + window.location.host);
+	console.log("CO2 : +1 = " + localStorage[c_localStorageKey_co2]);
 
 	//html5 video
     var video = $('video');
     video.on('playing', function(){
+		AddCo2(10);
 		console.log("CO2 : PLAYING VIDEO DETECTED");
+		console.log("CO2 : +10 = " + localStorage[c_localStorageKey_co2]);
     });
     video.on('pause', function(){
 		console.log("CO2 : STOPPED VIDEO DETECTED");
     });
 	
 })
+
