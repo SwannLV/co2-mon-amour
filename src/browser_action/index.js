1a9680179a6ed9af2f6e47408fda2bf3e2f97d56
swannlv <le.vigouroux.swann@gmail.com>
2016-07-13 11:41:17 +0200

    753

--- src/browser_action/index.js
+++ src/browser_action/index.js
@@ -8,5 +8,11 @@ $(function(){
 
   console.log(localStorage["co2"])
   $("#co2-count").html("XXX gCO2");
+  
+  chrome.storage.local.get("co2Count", function (obj) { 
+		if(isNaN(obj.co2Count)){ obj.co2Count = 0.0; SaveCo2Count(obj.co2Count); }
+		co2Count = obj.co2Count;
+		 $("#co2-count").html(parseInt(co2Count));
+	});	
 
 });
